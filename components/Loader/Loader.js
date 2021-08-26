9d11c2b74efcf059d91b4ce552f0b95bf1d9eaa2
gyd3ukc <70544293+gyd3ukc@users.noreply.github.com>
2021-08-27 01:36:09 +0300

    Add files via upload

---
+++ components/Loader/Loader.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { Text, View, StyleSheet } from 'react-native'
+
+const Loader = () => {
+    return (
+        <View >
+            <Text>Loading</Text>
+        </View>
+    )
+}
+
+export default Loader;
